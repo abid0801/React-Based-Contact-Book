b7547d6068c6fd768417f0ab366524e1ad5ca51f
abid0801 <abid.hassan@northsouth.edu>
2021-11-27 15:59:12 +0600

    Route Sturctes ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { ToastContainer } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = ()=> {
   return (
     <div className="App">
-      <h1>Redux contact</h1>
+      <ToastContainer/>
+      <Navbar/>
+      <Switch>
+          <Route exact path ="/" component= {()=><Home/>} />
+          <Route exact path ="/add">
+              <h1>I am add compoents</h1>
+          </Route>
+          
+      </Switch>
+
+    
     </div>
   );
 }
